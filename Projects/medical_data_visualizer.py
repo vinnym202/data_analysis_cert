4ab00b5a1cd201070c833de7df51ddce6f4b66b5
vinnym202 <vinnym202@gmail.com>
2023-03-01 15:37:44 -0500

    Final. All tests passed.

--- Projects/medical_data_visualizer.py
+++ Projects/medical_data_visualizer.py
@@ -16,19 +16,19 @@ df['gluc'] = np.where(df['gluc'] > 1, 1, 0)
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
+    df_cat['total'] = 1
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_index = False).count()
     
 
     # Draw the catplot with 'sns.catplot()'
-
+    fig = sns.catplot(y = 'total', x = 'variable', data = df_cat, kind = 'bar', col = 'cardio', hue ='value')
 
 
     # Get the figure for the output
-    fig = None
-
+    fig = fig.fig
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
@@ -38,21 +38,25 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    mask = (df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975))
+    df_heat = df[mask]
 
     # Calculate the correlation matrix
-    corr = None
+    corr = df_heat.corr()
 
     # Generate a mask for the upper triangle
-    mask = None
+    mask = np.triu(corr)
 
 
 
     # Set up the matplotlib figure
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(10,8))
 
     # Draw the heatmap with 'sns.heatmap()'
-
+    ax.tick_params(axis='x', labelsize=9)
+    ax.tick_params(axis='y', labelsize=9)
+    cmap = sns.color_palette("icefire", as_cmap=True)
+    sns.heatmap(corr, ax=ax, mask=mask, annot=True, cmap=cmap, annot_kws={'fontsize': 9}, fmt='.1f', center=0, linewidths=.5)
 
 
     # Do not modify the next two lines
