46e740d9ef8149eaf1cb4dfaaae44e252ffcc520
vinnym202 <vinnym202@gmail.com>
2023-03-01 19:06:26 -0500

    Line plot and Bar plot complete. Untested

--- Projects/time_series_visualizer.py
+++ Projects/time_series_visualizer.py
@@ -5,15 +5,19 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('/home/vinny/Code/Python/data_analysis_cert/Projects/fcc-forum-pageviews.csv', parse_dates=['date'], index_col='date')
 
 # Clean data
-df = None
+df = df.loc[(df['value'] <= df['value'].quantile(0.975)) & (df['value'] >= df['value'].quantile(0.025))]
 
 
 def draw_line_plot():
     # Draw line plot
-
+    fig, ax = plt.subplots(figsize=(16,5))
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    plt.plot(df, color='red')
 
 
 
@@ -24,10 +28,24 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.copy()
+    df_bar = df_bar.reset_index()
+    df_bar['date'] = pd.to_datetime(df_bar['date'])
+    df_bar['years'] = df_bar['date'].dt.year
+    df_bar['months'] = df_bar['date'].dt.month
+    months_named = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May',
+                    6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October',
+                    11: 'November', 12: 'December'}
+    order = ['January', 'February', 'March', 'April', 'May', 'June',
+                'July', 'August', 'September', 'October', 'November', 'December']
+    df_bar['months'] = pd.Categorical(df_bar['months'].map(months_named), categories=order, ordered=True)
+    df_bar = df_bar.groupby(['years', 'months'])['value'].mean()
+    df_bar = df_bar.unstack()
 
     # Draw bar plot
-
+    fig = df_bar.plot(kind='bar', figsize=(10,6))
+    plt.xlabel('Years')
+    plt.ylabel('Average Page Views')
 
 
 
